31beedf6b456b57af5ae10cf09e922a156a05289
Bhumi Singla <bhumi321a@gmail.com>
2026-02-07 21:37:25 +0530

    fixed token

--- server/config/token.js
+++ server/config/token.js
@@ -7,8 +7,8 @@ export const generateToken = async (userId, res) => {
 
   res.cookie("jwt", token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production", // true on deployed HTTPS
-    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+    secure: true,
+    sameSite: "none",
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
   });
 
--- server/middlewares/auth.middleware.js
+++ server/middlewares/auth.middleware.js
@@ -4,6 +4,8 @@ import jwt from 'jsonwebtoken'
 export const protectRoute = async(req,res,next) =>{
    try {
     const token = req.cookies.jwt
+    console.log("Cookies:", req.headers.cookie);
+    console.log(token)
     if(!token){
         return res.status(401).json({message: "Unauthorized user.."})
     }
@@ -11,6 +13,7 @@ export const protectRoute = async(req,res,next) =>{
     if(!decoded){
         return res.status(401).json({message: "Unauthorized user.."})
     }
+    console.log(decoded)
     const user = await User.findById(decoded.userId).select("-password")
     if(!user){
         return res.status(404).json({message: "User not found."})
