38bac2587d31a3ba31842150e6ed970bb0d59bab
Bhumi Singla <bhumi321a@gmail.com>
2026-02-07 16:39:59 +0530

    corrected auth path

--- server/routes/message.routes.js
+++ server/routes/message.routes.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import { getChatPartners, getMessages, sendMessage,search, getAllChatPartners } from '../controllers/message.controllers.js'
-import { protectRoute } from '../middlewares/Auth.middleware.js'
+import { protectRoute } from '../middlewares/auth.middleware.js'
 import upload from '../middlewares/multer.js'
 
 const router = express.Router()
